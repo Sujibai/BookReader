4d3e487a45a09e80976b6db5ce9be4e5fb2801e3
Sujibai <douniuzhijian@foxmail.com>
2024-09-23 15:53:34 +0800

    update openapi doc with pydantic

--- backend/main.py
+++ backend/main.py
@@ -1,39 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import random
-# import midmsf
-
 
 app = FastAPI()
 
 # 添加 CORS 中间件
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # 允许所有来源的请求
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],  # 允许的 HTTP 方法
-    allow_headers=["*"],  # 允许所有请求头
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
 )
 
-def generate_random_chinese_string():
-    # 定义中文字符集
-    chinese_characters = ''.join(chr(random.randint(0x4e00, 0x9fff)) for _ in range(random.randint(2, 20)))
-    return chinese_characters
+class Item(BaseModel):
+    image: str
+    text: str
 
-@app.get("/api/data")
-async def root():
+def generate_random_chinese_string():
+    return ''.join(chr(random.randint(0x4e00, 0x9fff)) for _ in range(random.randint(2, 20)))
+
+@app.get("/api/data", response_model=list[Item], summary="获取随机数据", description="返回一个包含随机生成中文字符串和图像的列表。")
+async def get_data():
     data = []
     for _ in range(35):
-        item = {
-            'image': random.choice(['/300.jpg','/500.jpg','/600.jpg', '/400.jpg']),
-            'text': generate_random_chinese_string()
-        }
+        item = Item(
+            image=random.choice(['/300.jpg', '/500.jpg', '/600.jpg', '/400.jpg']),
+            text=generate_random_chinese_string()
+        )
         data.append(item)
     return data
-
-# MyMsf = midmsf.MidMsf()
-
-# @app.get("/api/options")
-# async def get_options():
-#     options = MyMsf.get_options()
-#     return options
